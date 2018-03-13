d68112b1c58504b5eede4ce584b8bca6499451f9
liuxiaoguang <vincexgliu@gamil.com>
2018-03-13 16:17:43 +0800

    test to be finished

---
+++ json_lib/chapter1/test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "leptjson.h"
+
+static int main_ret = 0;
+static int test_count = 0;
+static int test_pass = 0;
+
+#define EXPECT_EQ_BASE(equality, expect, actual, format)
+    do{
+        pass;
+    }while(0)
+
+
