82af04f4a47f86156bfd1661026ad64630a9cc34
Alan Chen <a541203951@163.com>
2018-03-22 17:10:59 +0800

    struct

---
+++ c_fundemental/chapter_6/frequency_count.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+
+#define MAXWORD 100
+
+
+struct tnode {
+    char *word;
+    int count;
+    struct tnode *left;
+    struct tnode *right;
+};
+
+main()
+{
+    struct tnode *root;
+    char word[MAXWORD];
+
+    root = NULL;
+    while(getword(word, MAXWORD) != EOF)
+        if (isalpha(word[0]))
+            root = addtree(root , word);
+    treeprint(root);
+    return 0;
+}
+
+struct treenode *addtree(struct tnode *p, char *w)
+{
+    int cond;
+    if (p == NULL){
+        p = talloc();
+        p->word = strdup(w);
+        p->count = 1;
+        p->left = p->right = NULL;
+    }else if ((cond = strcmp(w, p->word)) == 0)
+        p->count++;
+    else if (cond < 0) /* less than into left subtree */
+        p->left = addtree(p->left, w);
+    else
+        p->right = addtree(p->right, w);
+    return p;
+}
+
+void treeprint(struct tnode *p)
+{
+    if (p != NULL) {
+        treeprint(p->left);
+        printf("%4d %s\n", p->count, p->word);
+        treeprint(p->right);
+    }
+}
+
+struct tnode *talloc(void)
+{
+    return (struct node *) malloc(sizeof(struct tnode));
+}
+
+char strdup(char *s)
+{
+    char *p;
+    p = (char *) malloc(strlen(s)+1);
+    if (p != NULL)
+        strcpy(p, s);
+    return p;
+}
